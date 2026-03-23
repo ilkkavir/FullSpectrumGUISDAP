361e66b7da8e1c5f27865797b96ed26017d1ce12
tiku12345 <tiku.bag@gmail.com>
2026-03-23 12:39:36 +0200

    Modified apriorimodel_plasmaline.m

--- apriorimodel_plasmaline.m
+++ apriorimodel_plasmaline.m
@@ -1,74 +1,83 @@
 function [apriori2,apriorierror2] = apriorimodel_plasmaline(apriori,apriorierror,heights,fit_altitude)
-%
-%
 % [apriori2,apriorierror2] = apriorimodel_plasmaline(apriori,apriorierror,heights,fit_altitude)
 %
-% Ne prior from plasma line data and O+ ion fraction fit when plasma lines are available. The default GUISDAP prior otherwise. 
-%
-% set iono_model='plasmaline' in GUISDAP to activate this function. 
-%
-% INPUT:
-%        apriori       initial prior parameters as returned ionomodel_bafim
-%        apriorierror  prior errors calculated in ionoomdel. The matrix is not used in this version of bafim.
-%        heights       centre points of height gates
-%        fit_altitude  the fit_altitude matrix used internally in GUISDAP
-%
-% OUTPUT:
-%        apriori2      modified prior values with Ne from plasma lines when available, and O+ ion fraction fit enabled in these cases. 
-%        apriorierror2 standard deviations of the prior values. 
-% 
-%
-%
-% Ilkka Virtanen & Tikemani Bag, 2026-
-%
-    
-    % global variables used in BAFIM. We will probably need only d_time in this case. 
-    global r_param r_error r_res r_status d_time path_GUP result_path v_Boltzmann
+% Ne prior from plasma line data and O+ ion fraction fit.
 
-    % copy the original prior model
+    % Load the plasma line data
+    load("/home/tibag_temp/Desktop/IONTRACE/GUISDAP_OUT/Plasma_line/plasma_30Jan2022_beata_2_2_time_2022-01-30.mat")
+    global d_time 
+    
     apriori2 = apriori;
     apriorierror2 = apriorierror;
 
-    % The variables
-    %
-    % heights        an array of height gate center point altitudes (km)
-    % apriori2       a length(heights) x N array of prior values. The first six columns are for Ne, Ti, Tr=Te/Ti, Coll, Vi, and [O+].
-    % apriorierror2  an array of standard deviations for the prior values.
-    % fit_altitude   included for compatibility with other prior models. Not needed here.
-    % d_time         start and end time of the integration period. 
+    %% Physical Constants
+    c = 299792458;
+    eps0 = 8.854187e-12;
+    e_charge = 1.602176e-19;
+    m_e = 9.109383e-31;
+    k_B = 1.380649e-23;
+    f_radar = 933e6; 
+    k_radar = 2 * pi * f_radar / c;
+    k_plasma_line = 2 * k_radar; 
 
+    % Height Gate
+    gate_size = 3; % km
+    half_gate = gate_size / 2;
+    low_e = heights - half_gate;
+    upp_e = heights + half_gate;
 
-    % Then replace the Ne prior values and relax the O+ fraction prior standard deviation when plasma line data are available as explained below
-    % 
-    % 1. find all altitudes from which we have plasma line data during the integration period (from d_time(1,:) to d_time(2,:))
-    % 2. At each gate (ipl):  If there is plasma line data from gate ipl, replace values in apriori2 and apriorierror2 as follows
-    %
-    %    - apriori2(ipl,1) = <Ne from plasma lines>
-    %    - apriorierror2(ipl,1) = <width of the plasma line>  (in units of Ne)
-    %    - apriorierror2(ipl,6) = 0.5
+    % Check if plasma line variables exist in the loaded .mat
+    if ~exist('peak_alt_fit_all', 'var'), return; end
 
-    % Unfortunately, edges of the height gates are not easily available. You will need to find  approximate gate edges from the center points in the heights array.
-    %
-    % Averaging will be needed if there are many plasma lines from one height gate. This may happen, because finer altitude resolution is used for the plasma line analysis.
-    %
-    % We can first assume that the plasma line frequency shift is equal to the local plasma frequency. 
-    % To include the Debye legth correction to the plasma line frequency (See equation 1 of Fredriksen et al., 1989), you can use the IRI Te, which can be calculated as apriori(:,2).*apriori(:,3).
-    %
-    % Center frequencies of the plasma line channels for different EISCAT experiments and their versions
-    %
-    % Tromso UHF radar
-    % beata 1.0  -5.0 MHz
-    % beata 2.0  -3.6 MHz, -6.0 MHz, -8.4 MHz,
-    % beata 2.1   3.6 MHz, -3.6 MHz, -6.0 MHz
-    % beata 2.2  -2.5 MHz, -4.9 MHz
-    % beata 2.3  -3.85 MHz
-    %
-    % There are more, including many ESR experiments in particular, but will need to investigate a bit to find the frequencies. 
-    % 
-    % 
-    % 
-    %
+    % Convert d_time to datenum for filtering
+    guisdap_start = datenum(d_time(1,1:6));
+    guisdap_end   = datenum(d_time(2,1:6));
+
+    mat_start = datenum(time_start);
+    mat_end   = datenum(time_end);
     
+    % Filter plasma line by current integration time (60 second)
+    time_idx = find(mat_start >= guisdap_start - 1/60 & mat_end <= guisdap_end + 1/60);
+
+    if isempty(time_idx), return; end
     
+    % Get plasma line data for this specific time interval
+    pl_alt = peak_alt_fit_all(time_idx);
+    pl_frq = peak_frq_fit_all(time_idx);
+    pl_df  = DF_fit_all(time_idx);
+
+    % use loop
+    num_gates = size(apriori, 1);
+    
+    for ipl = 1:num_gates
+        
+        % Find all plasma line peaks that fall within the current ion line gate
+        idx_gate = find(pl_alt >= low_e(ipl) & pl_alt < upp_e(ipl));
+        
+        if ~isempty(idx_gate)
+            
+            % Column 2: Ti, Column 3: Tr (Te/Ti)
+            Te_prior = apriori(ipl, 2) * apriori(ipl, 3);
+            
+            % Plasma line frequency shift (Hz); 6MHz (frequency band=2) 
+            fr = abs(pl_frq(idx_gate) - 6e6);  
+            
+            % Calculate Ne (m^-3) with Debye correction
+            % formula: f_p^2 = f_r^2 - 3*v_th^2*k^2 / (2pi)^2
+            % Ne = ( (fr*2*pi)^2 * m_e * eps0 / e^2 ) - (3 * k^2 * eps0 * k_B * Te / e^2)
+            term1 = ( (fr .* 2 .* pi).^2 .* m_e .* eps0 ) ./ (e_charge^2);
+            term2 = (3 * k_plasma_line^2 * eps0 * k_B * Te_prior) / (e_charge^2);
+            ne_points = term1 - term2;
+            
+            % Ne Prior (Column 1)
+            apriori2(ipl, 1) = mean(ne_points);
+            
+            % Ne Error (Column 1) in Ne unit; Derivative: dNe/dfr = (8 * pi^2 * m_e * eps0 / e^2) * fr
+            df = pl_df(idx_gate);
+            ne_err_points = (8 * pi^2 * m_e * eps0 / e_charge^2) .* fr .* df;
+            apriorierror2(ipl, 1) = mean(ne_err_points);
+            
+            apriorierror2(ipl, 6) = 0.5;
+        end
+    end
 end
-
